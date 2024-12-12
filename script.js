22596a257c7dd653b9c64c225de296f12d4d8e89
CoderDebajyoti <debajyoti.475@gmail.com>
2024-12-12 19:48:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-on
+function appendNumber(number) {
+    document.getElementById('display').value += number;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    let display = document.getElementById('display').value;
+    document.getElementById('display').value = display.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        let result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (e) {
+        document.getElementById('display').value = 'Error';
+    }
+}
